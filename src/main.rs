066feab006b00c88f45b43bdc8841ab9a2e073e6
Drake <dboul63@gmail.com>
2024-06-12 22:18:46 -0600

    Added input handling logic

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
-// use std::io;
+use std::io;
 
 fn main() {
-        println!("Welcome to the sysinfo database!");
+    println!("Welcome to the sysinfo database!");
+    // Loop to handle user input
+    loop {
         start_menu();
-        // let mut input = String::new();
+        let mut input = String::new();
 
-        // io::stdin()
-        //     .read_line(&mut input)
-        //     .expect("Failed to read input.");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input.");
         
-        // let input: u8 = match input.trim().parse() {
+        let input: u8 = match input.trim().parse() {
+            Ok(n) => n,
+            //TODO: add more useful error information by including an additional match statement for different types of errors
+            Err(_) => {
+                println!("Invalid input. Please enter a number in the range 1-5.");
+                continue;
+            }
+        };
 
-        //     Ok(n) => n,
+        match input {
+            1 => start_recording(),
+            2 => stop_recording(),
+            3 => view_records(),
+            4 => live_data_feed(),
+            5 => {
+                println!("Quitting Program...");
+                break;
 
-        //     Err(e) => {
+            }
+            _ => println!("Invalid input. Please enter a number in the range 1-5.")
+        };
 
-        //         println!("{}", e);
-        //         0
-        //     }
-        // };
-        
+    }
 }
 
 fn start_menu() {
@@ -31,7 +45,44 @@ fn start_menu() {
     println!("5.    Quit Program");
 }
 
-// fn start_recording_menu() {
-//     println!("We started recording your data, we will keep recording until you choose stop recording in the main menu.");
-//     println!("Press 'q' to go back to the main menu at anytime");
-// }
+// TODO: Write these 4 functions for the main logic of the program
+fn start_recording() {
+
+    println!("Starting recording... We will keep recording data for you until you stop recording");
+    println!("enter 'q' at any time to return to the main menu");
+
+    loop {
+        let mut input: String = String::new(); 
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Reading failed");
+
+        let input: char = match input.trim().parse() {
+            Ok(c) => c,
+            Err(_) => {
+                println!("Invlaid Input. Please enter 'q' to exit to the main menu.");
+                continue;
+            }
+        };
+
+        match input {
+            'q' => break,
+            _ => {
+                println!("Invlaid Input. Please enter 'q' to exit to the main menu.");
+                continue;
+            }
+        }
+    }
+}
+
+fn stop_recording() {
+    println!("Stopping recording...");
+}
+
+fn view_records() {
+    println!("Viewing records...");
+}
+
+fn live_data_feed() {
+    println!("Starting live data feed...");
+}
